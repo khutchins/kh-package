9728287c91220ea437da09e02d5fcece1857e24c
Kevin Hutchins <possessedcow@gmail.com>
2021-01-07 20:48:34 -0800

    Add conversation sample.

---
+++ Samples~/TextBox/Scripts/SimpleTextPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KH.Texts;
+
+public class SimpleTextPlayer : MonoBehaviour {
+    [TextArea]
+    public string Line = "<color=blue><speed=.05>(What<i><speed=.5><color=white> will </color></speed></i>happen next<pause=.5/>?)</color><shake=0.5/>";
+
+    void Start() {
+        TextAnimator.SharedAnimator.PlayText("Kevin", Color.white, Line);
+    }
+}
