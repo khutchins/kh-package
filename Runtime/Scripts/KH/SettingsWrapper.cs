c8e2df4ff576ae158fc45f277916e423ee12ebe6
Kevin Hutchins <possessedcow@gmail.com>
2021-01-07 19:16:35 -0800

    Add SettingsWrapper.

---
+++ Runtime/Scripts/KH/SettingsWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KH {
+    public static class SettingsWrapper {
+        static readonly string LOOK_SPEED = "_ls";
+
+		public static float LookSpeed { 
+			get => (float)WorldState.Shared.GetSetting(LOOK_SPEED, 1.0f); 
+			set => WorldState.Shared.SetSetting(LOOK_SPEED, value); 
+		}
+	}
+}
