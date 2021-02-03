9cb767b1f94b4a751eca4f63ed3de33ec83ca765
Kevin Hutchins <possessedcow@gmail.com>
2021-02-03 07:30:30 -0800

    Make button config instance variables readonly.

--- Runtime/Scripts/KH/UI/ButtonConfig.cs
+++ Runtime/Scripts/KH/UI/ButtonConfig.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 namespace KH.UI {
 	public class ButtonConfig : PanelObjectConfig {
-		public string DisplayText;
-		public ButtonPressedHandler Handler;
+		public readonly string DisplayText;
+		public readonly ButtonPressedHandler Handler;
 
 		public ButtonConfig(string key, string displayText, System.Action<GameObject> creationCallback, ButtonPressedHandler handler) : base(key, creationCallback) {
 			DisplayText = displayText;
