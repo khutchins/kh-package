96e08383c32b31f0d4a1f27a3aa74e4d24f4e178
Kevin Hutchins <possessedcow@gmail.com>
2021-02-10 07:16:55 -0800

    Add URL opener.

---
+++ Runtime/Scripts/KH/URLOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KH {
+	public class URLOpener : MonoBehaviour {
+		public string URL = "https://khutchins.itch.io";
+
+		public void OpenURL() {
+			// WebGL will replace the game window with the loaded page,
+			// which is less than ideal.
+			if (Application.platform != RuntimePlatform.WebGLPlayer) {
+				Application.OpenURL(URL);
+			}
+		}
+	}
+}
