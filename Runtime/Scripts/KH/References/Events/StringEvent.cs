588f236bb54438dc192715c6bf31029d3c6f6914
Kevin Hutchins <possessedcow@gmail.com>
2021-04-08 07:38:37 -0700

    Add string reference and event.

---
+++ Runtime/Scripts/KH/References/Events/StringEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace KH.References {
+	public class StringEvent : ValueEvent<int, StringReference> { }
+}
---
+++ Runtime/Scripts/KH/References/StringReference.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KH.References {
+	[CreateAssetMenu(menuName = "KH/Reference/String")]
+	public class StringReference : ValueReference<int> { }
+}
