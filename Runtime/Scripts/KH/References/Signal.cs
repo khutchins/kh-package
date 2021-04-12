bab6ef63f190fb54201599c65685475b2d9f8743
Kevin Hutchins <possessedcow@gmail.com>
2021-04-11 17:10:37 -0700

    Make signal creatable.

--- Runtime/Scripts/KH/References/Signal.cs
+++ Runtime/Scripts/KH/References/Signal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace KH.References {
+	[CreateAssetMenu(menuName = "KH/Reference/Signal")]
 	public class Signal : ScriptableObject {
 
 		public delegate void SignalRaised();
