2e62312e1804f320e42e8cfbb87c668f5b49a322
Kevin Hutchins <possessedcow@gmail.com>
2021-09-10 17:20:00 -0700

    Change SystemRandom instantiation to use tick count instead of instantiating a new system random for performance. From the documentation, it's implied they do this internally anyway. Also added some documentation.

--- Runtime/Scripts/KH/Rand/SystemRandom.cs
+++ Runtime/Scripts/KH/Rand/SystemRandom.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 namespace KH.Rand {
 	/// <summary>
-	/// Wrapper of System.Random that subclasses IRandom.
+	/// Wrapper of System.Random that subclasses IRandom. Note that this could potentially change across .NET versions and across platforms, depending on MS's whims.
+	/// 
+	/// From the docs here https://docs.microsoft.com/en-us/dotnet/api/system.random?redirectedfrom=MSDN&view=net-5.0: The implementation of the random number generator in the Random class isn't guaranteed to remain the same across major versions of the .NET Framework. As a result, you shouldn't assume that the same seed will result in the same pseudo-random sequence in different versions of the .NET Framework.
 	/// </summary>
 	public class SystemRandom : IRandom {
 
@@ -17,16 +19,25 @@ namespace KH.Rand {
 
 		public override object RawSeed => _rawSeed;
 
+		/// <summary>
+		/// System.Random RNG seeded with Environment.TickCount.
+		/// </summary>
 		public SystemRandom() {
-			_rawSeed = _seed = new System.Random().Next();
+			_rawSeed = _seed = Environment.TickCount;
 			_random = new System.Random(_seed);
 		}
 
+		/// <summary>
+		/// System.Random RNG seeded with the given seed.
+		/// </summary>
 		public SystemRandom(int seed) {
 			_rawSeed = _seed = seed;
 			_random = new System.Random(_seed);
 		}
 
+		/// <summary>
+		/// System.Random RNG seeded with the given seed string hashed to get an int.
+		/// </summary>
 		public SystemRandom(string seedStr) {
 			_rawSeed = seedStr;
 			_seed = StringToSeed(seedStr);
