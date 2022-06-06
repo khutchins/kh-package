70e7815c6df86a94d7ee39ef577e1bfd62a85665
Kevin Hutchins <possessedcow@gmail.com>
2022-06-06 08:23:45 -0700

    Let ASHelper create without scheduling a destroy, for potential pooling.

--- Runtime/Scripts/KH/Audio/ASHelper.cs
+++ Runtime/Scripts/KH/Audio/ASHelper.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class ASHelper {
-	public static AudioSource PlayClipAtPoint(AudioClip clip, Vector3 pos, bool is2D, float volume = 1f, float pitch = 1f) {
+	public static AudioSource PlayClipAtPoint(AudioClip clip, Vector3 pos, bool is2D, float volume = 1f, float pitch = 1f, bool shouldDestroy = true) {
 		GameObject audioGameObject = new GameObject("TempAudio");
-		audioGameObject.transform.position = pos;
 		AudioSource audioSource = audioGameObject.AddComponent<AudioSource>();
-		audioSource.clip = clip;
-		audioSource.volume = volume;
-		audioSource.pitch = pitch;
-		audioSource.spatialBlend = is2D ? 0 : 1;
+		RepurposeAudioSource(audioSource, clip, pos, is2D, volume, pitch);
 		audioSource.Play();
-		UnityEngine.Object.Destroy(audioGameObject, clip.length / Mathf.Max(0.001f, pitch));
+		if (shouldDestroy) Object.Destroy(audioGameObject, clip.length / Mathf.Max(0.001f, pitch));
 		return audioSource;
+	}
+
+	public static void RepurposeAudioSource(AudioSource source, AudioClip clip, Vector3 pos, bool is2D, float volume = 1f, float pitch = 1f) {
+		source.gameObject.transform.position = pos;
+		source.clip = clip;
+		source.volume = volume;
+		source.pitch = pitch;
+		source.spatialBlend = is2D ? 0 : 1;
 	}
 }
