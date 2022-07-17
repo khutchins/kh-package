ecdc7240d130cc5b09947b1c879b9e3286cc83f4
Kevin Hutchins <possessedcow@gmail.com>
2022-07-17 15:14:48 -0700

    Allow setting an int with BoolReceptor, and option select ref scripts for int and bools.

--- Runtime/Scripts/KH/References/Receptors/BoolReceptor.cs
+++ Runtime/Scripts/KH/References/Receptors/BoolReceptor.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace KH.References {
-	public class BoolReceptor : ValueReceptor<bool, BoolReference> { }
+	public class BoolReceptor : ValueReceptor<bool, BoolReference> {
+		public void UpdateValue(int newValue) {
+			Reference?.SetValue(newValue == 0 ? false : true);
+		}
+	}
 }
