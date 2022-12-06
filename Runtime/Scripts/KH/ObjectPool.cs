f68eecb89b09f893e852221dd838d348bf587abb
Kevin Hutchins <possessedcow@gmail.com>
2022-12-05 20:43:34 -0800

    Add object pooling.

---
+++ Runtime/Scripts/KH/ObjectPool.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+    public static ObjectPool Instance;
+	private static readonly bool DEBUG = false;
+
+	private Dictionary<GameObject, List<GameObject>> _pool = new Dictionary<GameObject, List<GameObject>>();
+
+	private void Awake() {
+		Instance = this;
+	}
+
+	public GameObject GetObject(GameObject prefab) {
+		List<GameObject> list;
+		if (!_pool.ContainsKey(prefab)) {
+			list = new List<GameObject>();
+			_pool[prefab] = list;
+		} else {
+			list = _pool[prefab];
+		}
+
+		foreach (GameObject item in list) {
+			if (!item.activeInHierarchy) {
+				if (DEBUG) Debug.LogFormat("Cache hit for {0}", prefab);
+				return item;
+			}
+		}
+		GameObject obj = Instantiate(prefab);
+		obj.SetActive(false);
+		list.Add(obj);
+		if (DEBUG) Debug.LogFormat("Expanded pool for {0} to {1}", prefab, list.Count);
+		return obj;
+	}
+
+	public GameObject GetObject(GameObject prefab, Vector3 loc, Quaternion rot, Transform parent = null) {
+		GameObject go = GetObject(prefab);
+		go.transform.SetParent(parent);
+		go.transform.position = loc;
+		go.transform.rotation = rot;
+		return go;
+	}
+}
