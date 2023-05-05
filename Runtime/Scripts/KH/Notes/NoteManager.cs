f992469e2314330b311fc7eb48e6f2f2c7905755
Kevin Hutchins <possessedcow@gmail.com>
2023-05-04 23:18:58 -0700

    Pull some code out of NoteManager to allow handling duplicate code with BookManager.

--- Runtime/Scripts/KH/Notes/NoteManager.cs
+++ Runtime/Scripts/KH/Notes/NoteManager.cs
@@ -10,31 +10,28 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace KH.Notes {
-    public class NoteManager : MonoBehaviour, IMenu {
-        public MenuInputMediator InputMediator;
-        public Button Previous;
-        public Button Next;
-        public Button Exit;
-        public TextMeshProUGUI Text;
-        public Image BackgroundImage;
-        public BoolReference Paused;
-        public NoteReference NoteReference;
-        public bool PausesGame = true;
+    public class NoteManager : PaginatedTextManager {
+        [SerializeField] TMP_Text Text;
+        [SerializeField] Image BackgroundImage;
+        [SerializeField] NoteReference NoteReference;
 
         private Note _currentNote;
         private int _currentIdx;
-        private GameObject _defaultObject;
-        private GameObject _cachedSelection;
-        private Canvas _canvas;
-        private bool _active;
 
-        private void Awake() {
-            Previous.onClick.AddListener(PreviousPressed);
-            Next.onClick.AddListener(NextPressed);
-            Exit.onClick.AddListener(ExitPressed);
-            _canvas = GetComponent<Canvas>();
-            _canvas.enabled = false;
-        }
+        protected override bool CanGoBack => 
+            _currentIdx > 0 && 
+            _currentNote != null && 
+            _currentNote.Pages != null && 
+            _currentNote.Pages.Length > 0;
+
+        protected override bool CanGoForward => 
+            _currentNote != null && 
+            _currentNote.Pages != null && 
+            _currentIdx < _currentNote.Pages.Length - 1;
+
+        protected override AudioEvent DisplayAudio => NoteReference == null ? null : (NoteReference.Value == null ? null : NoteReference.Value.PickUpAudio);
+        protected override AudioEvent ForwardAudio => NoteReference == null ? null : (NoteReference.Value == null ? null : NoteReference.Value.PageTurnAudio);
+        protected override AudioEvent BackAudio => NoteReference == null ? null : (NoteReference.Value == null ? null : NoteReference.Value.PageTurnAudio);
 
         private void OnEnable() {
             NoteReference.ValueChanged += ShowNote;
@@ -44,24 +41,33 @@ namespace KH.Notes {
             NoteReference.ValueChanged -= ShowNote;
         }
 
-        public void ShowNote(Note note) {
-            _currentNote = note;
-            if (_currentNote == null || _currentNote.Pages == null || _currentNote.Pages.Length == 0) {
-                if (_active) {
-                    MenuStack.Shared.PopAndCloseMenu(this);
-                }
-                if (_currentNote != null) {
-                    // Bad note that we have to clear. Wait a frame, since 
-                    // doing this online can break some behavior (for instance,
-                    // registering a listener on the note reference after setting
-                    // it, which this would happen before).
-                    ClearNoteOnDelay();
-                }
+        protected override void OnDidDisplay() {
+            if (_currentNote != null && (_currentNote.Pages == null || _currentNote.Pages.Length == 0)) {
+                // Bad note that we have to clear. Wait a frame, since 
+                // doing this inline can break some behavior (for instance,
+                // registering a listener on the note reference after setting
+                // it, which this would happen before).
+                ClearNoteOnDelay();
                 return;
             }
-            MenuStack.Shared.PushAndShowMenu(this);
-            _currentNote.PickUpAudio?.PlayOneShot();
-            SetPage(0, false);
+
+            if (_currentNote == null) return;
+            BackgroundImage.enabled = _currentNote.Image != null;
+            BackgroundImage.sprite = _currentNote.Image;
+            SetPage(0);
+        }
+
+        protected override void OnDidClose() {
+            NoteReference.SetValue(null);
+        }
+
+        void ShowNote(Note note) {
+            _currentNote = note;
+            if (note != null) {
+                Display();
+            } else {
+                Close();
+            }
         }
 
         private void ClearNoteOnDelay() {
@@ -73,85 +79,26 @@ namespace KH.Notes {
             NoteReference.SetValue(null);
         }
 
-        private void SetPage(int idx, bool allowAudio = true) {
+        private void SetPage(int idx) {
             if (idx >= _currentNote.Pages.Length) idx = _currentNote.Pages.Length - 1;
             if (idx < 0) idx = 0;
 
-            if (allowAudio) _currentNote.PageTurnAudio?.PlayOneShot();
+            // Bad note with no pages.
+            if (idx >= _currentNote.Pages.Length) {
+                Text.text = "";
+                return;
+            }
 
-            bool back = _currentIdx > idx;
             _currentIdx = idx;
-
             Text.text = _currentNote.Pages[_currentIdx];
-            BackgroundImage.enabled = _currentNote.Image != null;
-            BackgroundImage.sprite = _currentNote.Image;
-
-            UpdateButtons(idx != 0, idx != _currentNote.Pages.Length - 1, back);
         }
 
-        private void UpdateButtons(bool previous, bool next, bool wentBack) {
-            Exit.gameObject.SetActive(!next);
-            Previous.gameObject.SetActive(previous);
-            Next.gameObject.SetActive(next);
-
-            List<Selectable> objs = new List<Selectable>();
-            if (previous) objs.Add(Previous);
-            objs.Add(next ? Next : Exit);
-
-            EventSystem.current.SetSelectedGameObject(null);
-
-            // Hook up navigation with elements with selectable objects.
-            for (int i = 0; i < objs.Count; i++) {
-                // Make new one to avoid potential property strangeness.
-                Navigation navigation = new Navigation();
-                navigation.mode = Navigation.Mode.Explicit;
-                navigation.selectOnLeft = i > 0 ? objs[i - 1] : null;
-                navigation.selectOnRight = i < objs.Count - 1 ? objs[i + 1] : null;
-                objs[i].navigation = navigation;
-            }
-
-            GameObject selected = wentBack ? objs[0].gameObject : objs[objs.Count - 1].gameObject;
-            _defaultObject = selected;
-            EventSystem.current.SetSelectedGameObject(selected);
-        }
-
-        private void Update() {
-            if (!_active) return;
-            if (_defaultObject != null && EventSystem.current.currentSelectedGameObject == null
-                && (Mathf.Abs(InputMediator.UIX()) > 0.1 || Mathf.Abs(InputMediator.UIY()) > 0.1)) {
-                EventSystem.current.SetSelectedGameObject(_defaultObject);
-            }
-        }
-
-        void NextPressed() {
-            SetPage(_currentIdx + 1);
-        }
-
-        void PreviousPressed() {
+        protected override void GoBack() {
             SetPage(_currentIdx - 1);
         }
 
-        void ExitPressed() {
-            NoteReference.SetValue(null);
-
-            MenuStack.Shared.PopAndCloseMenu(this);
-        }
-
-        public MenuAttributes GetMenuAttributes() {
-            return PausesGame ? MenuAttributes.StandardPauseMenu() : MenuAttributes.StandardNonPauseMenu();
-        }
-
-        public void SetMenuUp(bool newUp) {
-            _canvas.enabled = newUp;
-        }
-
-        public void SetMenuOnTop(bool isTop) {
-            if (!isTop) {
-                _cachedSelection = EventSystem.current.currentSelectedGameObject;
-                EventSystem.current.SetSelectedGameObject(null);
-            }
-            if (isTop && _cachedSelection != null) EventSystem.current.SetSelectedGameObject(_cachedSelection);
-            _active = isTop;
+        protected override void GoForward() {
+            SetPage(_currentIdx + 1);
         }
     }
 }
---
+++ Runtime/Scripts/KH/Notes/PaginatedTextManager.cs
@@ -0,0 +1,140 @@
+using KH.Audio;
+using KH.Input;
+using Ratferences;
+using Menutee;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace KH.Notes {
+    public abstract class PaginatedTextManager : MonoBehaviour, IMenu {
+        [SerializeField] MenuInputMediator InputMediator;
+        [SerializeField] Button Previous;
+        [SerializeField] Button Next;
+        [SerializeField] Button Exit;
+        [SerializeField] bool PausesGame = true;
+
+        private GameObject _defaultObject;
+        private GameObject _cachedSelection;
+        private Canvas _canvas;
+        private bool _active;
+
+        private void Awake() {
+            Previous.onClick.AddListener(PreviousPressed);
+            Next.onClick.AddListener(NextPressed);
+            Exit.onClick.AddListener(ExitPressed);
+            _canvas = GetComponent<Canvas>();
+            _canvas.enabled = false;
+        }
+
+        public void Display() {
+            if (_active) {
+                MenuStack.Shared.PopAndCloseMenu(this);
+            }
+            OnWillDisplay();
+            AudioEvent audio = DisplayAudio;
+            if (audio != null) audio.PlayOneShot();
+            MenuStack.Shared.PushAndShowMenu(this);
+            OnDidDisplay();
+            UpdateButtons(false);
+        }
+
+        protected virtual void OnWillDisplay() { }
+        protected virtual void OnDidDisplay() { }
+        protected virtual void OnWillClose() { }
+        protected virtual void OnDidClose() { }
+        protected abstract bool CanGoBack { get; }
+        protected abstract void GoBack();
+        protected abstract bool CanGoForward { get; }
+        protected abstract void GoForward();
+        protected virtual AudioEvent DisplayAudio { get => null; }
+        protected virtual AudioEvent CloseAudio { get => null; }
+        protected virtual AudioEvent ForwardAudio { get => null; }
+        protected virtual AudioEvent BackAudio { get => null; }
+
+        private void UpdateButtons(bool wentBack) {
+            UpdateButtons(CanGoBack, CanGoForward, wentBack);
+        }
+
+        private void UpdateButtons(bool previous, bool next, bool wentBack) {
+            Exit.gameObject.SetActive(!next);
+            Previous.gameObject.SetActive(previous);
+            Next.gameObject.SetActive(next);
+
+            List<Selectable> objs = new List<Selectable>();
+            if (previous) objs.Add(Previous);
+            objs.Add(next ? Next : Exit);
+
+            EventSystem.current.SetSelectedGameObject(null);
+
+            // Hook up navigation with elements with selectable objects.
+            for (int i = 0; i < objs.Count; i++) {
+                // Make new one to avoid potential property strangeness.
+                Navigation navigation = new Navigation();
+                navigation.mode = Navigation.Mode.Explicit;
+                navigation.selectOnLeft = i > 0 ? objs[i - 1] : null;
+                navigation.selectOnRight = i < objs.Count - 1 ? objs[i + 1] : null;
+                objs[i].navigation = navigation;
+            }
+
+            GameObject selected = wentBack ? objs[0].gameObject : objs[objs.Count - 1].gameObject;
+            _defaultObject = selected;
+            EventSystem.current.SetSelectedGameObject(selected);
+        }
+
+        private void Update() {
+            if (!_active) return;
+            if (_defaultObject != null && EventSystem.current.currentSelectedGameObject == null
+                && (Mathf.Abs(InputMediator.UIX()) > 0.1 || Mathf.Abs(InputMediator.UIY()) > 0.1)) {
+                EventSystem.current.SetSelectedGameObject(_defaultObject);
+            }
+        }
+
+        void NextPressed() {
+            if (CanGoForward) GoForward();
+            AudioEvent audio = ForwardAudio;
+            if (audio != null) audio.PlayOneShot();
+            UpdateButtons(false);
+        }
+
+        void PreviousPressed() {
+            if (CanGoBack) GoBack();
+            AudioEvent audio = BackAudio;
+            if (audio != null) audio.PlayOneShot();
+            UpdateButtons(false);
+        }
+
+        void ExitPressed() {
+            Close();
+        }
+
+        public void Close() {
+            if (!_active) return;
+            OnWillClose();
+            MenuStack.Shared.PopAndCloseMenu(this);
+            AudioEvent audio = CloseAudio;
+            if (audio != null) audio.PlayOneShot();
+            OnDidClose();
+        }
+
+        public MenuAttributes GetMenuAttributes() {
+            return PausesGame ? MenuAttributes.StandardPauseMenu() : MenuAttributes.StandardNonPauseMenu();
+        }
+
+        public void SetMenuUp(bool newUp) {
+            _canvas.enabled = newUp;
+        }
+
+        public void SetMenuOnTop(bool isTop) {
+            if (!isTop) {
+                _cachedSelection = EventSystem.current.currentSelectedGameObject;
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+            if (isTop && _cachedSelection != null) EventSystem.current.SetSelectedGameObject(_cachedSelection);
+            _active = isTop;
+        }
+    }
+}
