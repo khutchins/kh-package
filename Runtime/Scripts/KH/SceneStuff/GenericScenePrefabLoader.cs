cb1c67ad0d311a5e9c003c82bd1af6c6da2e3b68
Kevin Hutchins <possessedcow@gmail.com>
2023-06-29 17:53:39 -0700

    Generic implementation of ScenePrefabLoader that contains two types: menu and in-game. Not only does that work in and of itself with >90% of my games, it also is a good model for the rest of them.

---
+++ Runtime/Scripts/KH/SceneStuff/GenericScenePrefabLoader.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KH.SceneStuff {
+    [DefaultExecutionOrder(-500)]
+    public class GenericScenePrefabLoader : ScenePrefabLoader {
+        public enum MenuType {
+            InGameMenu,
+            MainMenu,
+            None
+        }
+
+        [SerializeField]
+        private MenuList[] MenuPrefabs;
+        [SerializeField]
+        private GameObject[] BonusPrefabs;
+
+        [Header("DON'T OVERRIDE ABOVE!")]
+
+        [SerializeField]
+        private MenuType _menuType;
+
+        private class GenericList<T> where T : System.Enum {
+            public T Type;
+            public GameObject[] Prefabs;
+
+            public static GameObject[] PrefabsForType(GenericList<T>[] items, T type) {
+                foreach (var item in items) {
+                    if (EqualityComparer<T>.Default.Equals(item.Type, type)) return item.Prefabs;
+                }
+                Debug.LogWarning("No items found for enum " + type);
+                return new GameObject[0];
+            }
+        }
+
+        [System.Serializable]
+        private class MenuList : GenericList<MenuType> { }
+
+        protected override void OnAfterLoadSceneScoped() {
+            LoadAllPrefabs(MenuList.PrefabsForType(MenuPrefabs, _menuType));
+            if (BonusPrefabs != null) {
+                LoadAllPrefabs(BonusPrefabs);
+            }
+        }
+    }
+}
