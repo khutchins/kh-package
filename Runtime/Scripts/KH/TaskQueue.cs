c99e8934ad7be2c8776c2a4fb19232002053aa4d
Kevin Hutchins <possessedcow@gmail.com>
2023-12-02 13:16:21 -0800

    Fixed TaskQueue/Manager not having namespace.

--- Runtime/Scripts/KH/TaskQueue.cs
+++ Runtime/Scripts/KH/TaskQueue.cs
@@ -2,125 +2,127 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Object that handles the processing of tasks in a queue,
-/// allowing coroutines to yield on a specific task being
-/// completed as well as waiting for all tasks.
-/// 
-/// It's designed for a single handler that coordinates
-/// parsing out the tasks to classes that can handle them,
-/// but it should work as long as handlers behave well and
-/// only grab tasks intended for them. TaskQueueManager is
-/// an implementation of the former.
-/// </summary>
-[CreateAssetMenu(menuName = "KH/TaskQueue", fileName = "TaskQueue")]
-public class TaskQueue : ScriptableObject {
-    public interface ITask {
-    }
-
-    public delegate void TaskAdded(TaskQueue queue);
-    public TaskAdded OnTaskAdded;
-    public delegate void QueueNoLongerEmpty(TaskQueue queue);
-    public QueueNoLongerEmpty OnQueueNoLongerEmpty;
-
-    private readonly Queue<ITask> _queue = new();
-    private int _lastIndex = -1;
-    private int _lastStartedTask = -1;
-    private int _lastFinishedTask = -1;
-    private bool _taskInProgress = false;
-
-    public bool TaskInProgress { get => _taskInProgress; }
-    public bool Empty { get => _queue.Count == 0; }
-
+namespace KH {
     /// <summary>
-    /// Enqueues the interaction and waits until the task is 
+    /// Object that handles the processing of tasks in a queue,
+    /// allowing coroutines to yield on a specific task being
+    /// completed as well as waiting for all tasks.
+    /// 
+    /// It's designed for a single handler that coordinates
+    /// parsing out the tasks to classes that can handle them,
+    /// but it should work as long as handlers behave well and
+    /// only grab tasks intended for them. TaskQueueManager is
+    /// an implementation of the former.
     /// </summary>
-    /// <param name="interact">Task to be enqueued.</param>
-    /// <returns>ID for the enqueued task.</returns>
-    public int Enqueue(ITask interact) {
-        _lastIndex++;
-        _queue.Enqueue(interact);
-        if (_queue.Count == 1) OnQueueNoLongerEmpty?.Invoke(this);
-        OnTaskAdded?.Invoke(this);
-        return _lastIndex;
-    }
+    [CreateAssetMenu(menuName = "KH/TaskQueue", fileName = "TaskQueue")]
+    public class TaskQueue : ScriptableObject {
+        public interface ITask {
+        }
 
-    /// <summary>
-    /// Removes all tasks from queue, clearing all the interactions and triggering
-    /// an end to all coroutines yielding on methods in this class.
-    /// </summary>
-    public void Clear() {
-        _taskInProgress = false;
-        while (!Empty) {
-            _queue.Dequeue();
+        public delegate void TaskAdded(TaskQueue queue);
+        public TaskAdded OnTaskAdded;
+        public delegate void QueueNoLongerEmpty(TaskQueue queue);
+        public QueueNoLongerEmpty OnQueueNoLongerEmpty;
+
+        private readonly Queue<ITask> _queue = new();
+        private int _lastIndex = -1;
+        private int _lastStartedTask = -1;
+        private int _lastFinishedTask = -1;
+        private bool _taskInProgress = false;
+
+        public bool TaskInProgress { get => _taskInProgress; }
+        public bool Empty { get => _queue.Count == 0; }
+
+        /// <summary>
+        /// Enqueues the interaction and waits until the task is 
+        /// </summary>
+        /// <param name="interact">Task to be enqueued.</param>
+        /// <returns>ID for the enqueued task.</returns>
+        public int Enqueue(ITask interact) {
+            _lastIndex++;
+            _queue.Enqueue(interact);
+            if (_queue.Count == 1) OnQueueNoLongerEmpty?.Invoke(this);
+            OnTaskAdded?.Invoke(this);
+            return _lastIndex;
+        }
+
+        /// <summary>
+        /// Removes all tasks from queue, clearing all the interactions and triggering
+        /// an end to all coroutines yielding on methods in this class.
+        /// </summary>
+        public void Clear() {
+            _taskInProgress = false;
+            while (!Empty) {
+                _queue.Dequeue();
+                _lastStartedTask++;
+                _lastFinishedTask++;
+            }
+            ResetTasksIndexes();
+        }
+
+        void ResetTasksIndexes() {
+            _lastIndex = -1;
+            _lastStartedTask = -1;
+            _lastFinishedTask = -1;
+        }
+
+        /// <summary>
+        /// Handles a task by taking in a task handler. Note that this does not
+        /// behave well if multiple tasks are handled concurrently (it's subject
+        /// to a race condition in task finished coroutines). There should only
+        /// be one handler, but if there's more than one, you can gate calling this
+        /// method based on the TaskInProgress property.
+        /// </summary>
+        /// <param name="taskHandler"></param>
+        public IEnumerator HandleTask(System.Func<ITask, IEnumerator> taskHandler) {
+            if (Empty) yield break;
+            _taskInProgress = true;
             _lastStartedTask++;
+            yield return taskHandler(_queue.Dequeue());
             _lastFinishedTask++;
+            _taskInProgress = false;
         }
-        ResetTasksIndexes();
-    }
 
-    void ResetTasksIndexes() {
-        _lastIndex = -1;
-        _lastStartedTask = -1;
-        _lastFinishedTask = -1;
-    }
-
-    /// <summary>
-    /// Handles a task by taking in a task handler. Note that this does not
-    /// behave well if multiple tasks are handled concurrently (it's subject
-    /// to a race condition in task finished coroutines). There should only
-    /// be one handler, but if there's more than one, you can gate calling this
-    /// method based on the TaskInProgress property.
-    /// </summary>
-    /// <param name="taskHandler"></param>
-    public IEnumerator HandleTask(System.Func<ITask, IEnumerator> taskHandler) {
-        if (Empty) yield break;
-        _taskInProgress = true;
-        _lastStartedTask++;
-        yield return taskHandler(_queue.Dequeue());
-        _lastFinishedTask++;
-        _taskInProgress = false;
-    }
-
-    /// <summary>
-    /// Handles all tasks by taking in a task handler. Note that this does not
-    /// behave well if multiple tasks are handled concurrently (it's subject
-    /// to a race condition in task finished coroutines). There should only
-    /// be one handler, but if there's more than one, you can gate calling this
-    /// method based on the TaskInProgress property.
-    /// </summary>
-    /// <param name="taskHandler"></param>
-    public IEnumerator HandleAllTasks(System.Func<ITask, IEnumerator> taskHandler) {
-        while (!Empty) {
-            yield return HandleTask(taskHandler);
+        /// <summary>
+        /// Handles all tasks by taking in a task handler. Note that this does not
+        /// behave well if multiple tasks are handled concurrently (it's subject
+        /// to a race condition in task finished coroutines). There should only
+        /// be one handler, but if there's more than one, you can gate calling this
+        /// method based on the TaskInProgress property.
+        /// </summary>
+        /// <param name="taskHandler"></param>
+        public IEnumerator HandleAllTasks(System.Func<ITask, IEnumerator> taskHandler) {
+            while (!Empty) {
+                yield return HandleTask(taskHandler);
+            }
         }
-    }
 
-    /// <summary>
-    /// Enqueues a task and waits until that task has started.
-    /// </summary>
-    public IEnumerator EnqueueAndAwaitTaskStarted(ITask task) {
-        int taskId = Enqueue(task);
-        yield return WaitUntilTaskStarted(taskId);
-    }
+        /// <summary>
+        /// Enqueues a task and waits until that task has started.
+        /// </summary>
+        public IEnumerator EnqueueAndAwaitTaskStarted(ITask task) {
+            int taskId = Enqueue(task);
+            yield return WaitUntilTaskStarted(taskId);
+        }
 
-    /// <summary>
-    /// Enqueues a task and waits until that task has finished.
-    /// </summary>
-    public IEnumerator EnqueueAndAwaitTaskFinished(ITask task) {
-        int taskId = Enqueue(task);
-        yield return WaitUntilTaskFinished(taskId);
-    }
+        /// <summary>
+        /// Enqueues a task and waits until that task has finished.
+        /// </summary>
+        public IEnumerator EnqueueAndAwaitTaskFinished(ITask task) {
+            int taskId = Enqueue(task);
+            yield return WaitUntilTaskFinished(taskId);
+        }
 
-    public IEnumerator WaitUntilTaskStarted(int taskId) {
-        while (taskId > _lastStartedTask) yield return null;
-    }
+        public IEnumerator WaitUntilTaskStarted(int taskId) {
+            while (taskId > _lastStartedTask) yield return null;
+        }
 
-    public IEnumerator WaitUntilTaskFinished(int taskId) {
-        while (taskId > _lastFinishedTask) yield return null;
-    }
+        public IEnumerator WaitUntilTaskFinished(int taskId) {
+            while (taskId > _lastFinishedTask) yield return null;
+        }
 
-    public IEnumerator WaitUntilEmpty() {
-        while (_queue.Count > 0 || _taskInProgress) yield return null;
+        public IEnumerator WaitUntilEmpty() {
+            while (_queue.Count > 0 || _taskInProgress) yield return null;
+        }
     }
 }
--- Runtime/Scripts/KH/TaskQueueManager.cs
+++ Runtime/Scripts/KH/TaskQueueManager.cs
@@ -1,73 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static TaskQueue;
+using static KH.TaskQueue;
 
-/// <summary>
-/// A simple implementation of a handler for the TaskQueue.
-/// Each task handler should register itself with the manager,
-/// which will automatically process all tasks once one is
-/// added to the queue.
-/// 
-/// Designed to be a per scene singleton. It'll clear the queue
-/// on awake and register itself as the singleton (hence the 
-/// earlier execution order), so it should be safe to register
-/// handlers on the instance in their own awake, as long as they
-/// don't also have an earlier execution order. If it comes
-/// across a task that it cannot handle, it will log a warning
-/// and drop it.
-/// </summary>
-[DefaultExecutionOrder(-1)]
-public class TaskQueueManager : MonoBehaviour {
-    [SerializeField] TaskQueue Queue;
+namespace KH {
+    /// <summary>
+    /// A simple implementation of a handler for the TaskQueue.
+    /// Each task handler should register itself with the manager,
+    /// which will automatically process all tasks once one is
+    /// added to the queue.
+    /// 
+    /// Designed to be a per scene singleton. It'll clear the queue
+    /// on awake and register itself as the singleton (hence the 
+    /// earlier execution order), so it should be safe to register
+    /// handlers on the instance in their own awake, as long as they
+    /// don't also have an earlier execution order. If it comes
+    /// across a task that it cannot handle, it will log a warning
+    /// and drop it.
+    /// </summary>
+    [DefaultExecutionOrder(-1)]
+    public class TaskQueueManager : MonoBehaviour {
+        [SerializeField] TaskQueue Queue;
 
-    private readonly Dictionary<System.Type, System.Func<ITask, IEnumerator>> _taskHandlers = new();
+        private readonly Dictionary<System.Type, System.Func<ITask, IEnumerator>> _taskHandlers = new();
 
-    public static TaskQueueManager INSTANCE;
+        public static TaskQueueManager INSTANCE;
 
-    private void Awake() {
-        Queue.Clear();
-        INSTANCE = this;
-    }
-
-    private void OnEnable() {
-        Queue.OnQueueNoLongerEmpty += QueueNotEmpty;
-        if (!Queue.Empty) QueueNotEmpty(Queue);
-    }
-
-    private void OnDisable() {
-        Queue.OnQueueNoLongerEmpty -= QueueNotEmpty;
-    }
-
-    public void AddHandler(System.Type taskType, System.Func<ITask, IEnumerator> handler) {
-        if (_taskHandlers.ContainsKey(taskType)) {
-            Debug.LogWarning($"Replacing handler for {taskType}.");
+        private void Awake() {
+            Queue.Clear();
+            INSTANCE = this;
         }
-        _taskHandlers[taskType] = handler;
-    }
 
-    public void RemoveHandler(System.Type taskType, System.Func<ITask, IEnumerator> handler) {
-        if (_taskHandlers.ContainsKey(taskType) || _taskHandlers[taskType] != handler) {
-            Debug.LogWarning($"Removing handler for {taskType} that wasn't registered or was replaced.");
-            return;
+        private void OnEnable() {
+            Queue.OnQueueNoLongerEmpty += QueueNotEmpty;
+            if (!Queue.Empty) QueueNotEmpty(Queue);
         }
-        _taskHandlers.Remove(taskType);
-    }
 
-    void QueueNotEmpty(TaskQueue queue) {
-        if (!queue.TaskInProgress) StartCoroutine(ClearAllTasks(queue));
-    }
-
-    IEnumerator TaskHandler(ITask task) {
-        if (_taskHandlers.ContainsKey(task.GetType())) {
-            yield return _taskHandlers[task.GetType()](task);
-        } else {
-            Debug.LogWarning($"No task handler for {task}.");
-            yield return null;
+        private void OnDisable() {
+            Queue.OnQueueNoLongerEmpty -= QueueNotEmpty;
         }
-    }
 
-    IEnumerator ClearAllTasks(TaskQueue queue) {
-        yield return queue.HandleAllTasks(TaskHandler);
+        public void AddHandler(System.Type taskType, System.Func<ITask, IEnumerator> handler) {
+            if (_taskHandlers.ContainsKey(taskType)) {
+                Debug.LogWarning($"Replacing handler for {taskType}.");
+            }
+            _taskHandlers[taskType] = handler;
+        }
+
+        public void RemoveHandler(System.Type taskType, System.Func<ITask, IEnumerator> handler) {
+            if (_taskHandlers.ContainsKey(taskType) || _taskHandlers[taskType] != handler) {
+                Debug.LogWarning($"Removing handler for {taskType} that wasn't registered or was replaced.");
+                return;
+            }
+            _taskHandlers.Remove(taskType);
+        }
+
+        void QueueNotEmpty(TaskQueue queue) {
+            if (!queue.TaskInProgress) StartCoroutine(ClearAllTasks(queue));
+        }
+
+        IEnumerator TaskHandler(ITask task) {
+            if (_taskHandlers.ContainsKey(task.GetType())) {
+                yield return _taskHandlers[task.GetType()](task);
+            } else {
+                Debug.LogWarning($"No task handler for {task}.");
+                yield return null;
+            }
+        }
+
+        IEnumerator ClearAllTasks(TaskQueue queue) {
+            yield return queue.HandleAllTasks(TaskHandler);
+        }
     }
 }
