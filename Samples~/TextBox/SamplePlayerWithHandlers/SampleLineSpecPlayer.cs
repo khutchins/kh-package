37e4ddf4169ed8d56a4660a2b30cae3b0292967b
Kevin Hutchins <possessedcow@gmail.com>
2024-02-08 21:23:05 -0800

    Reorganize conversation sample, add choice example, clean up multi text box example.

---
+++ Samples~/TextBox/SamplePlayerWithHandlers/SampleLineSpecPlayer.cs
@@ -0,0 +1,27 @@
+﻿using KH.Texts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLineSpecPlayer : MonoBehaviour
+{
+    [SerializeField] string[] Speakers;
+    [SerializeField] [TextArea] string[] Lines;
+
+    [SerializeField] LineSpecQueue Queue;
+
+    void Start() {
+        for (int i = 0; i < Lines.Length; i++) {
+            LineSpec spec = new LineSpec(
+                Speakers.Length > i ? Speakers[i] : "",
+                Lines[i],
+                i == Lines.Length - 1 ? LineCallback : null
+            );
+            Queue.Enqueue(spec);
+		}
+    }
+
+    void LineCallback() {
+        Debug.Log("Texts finished");
+	}
+}
