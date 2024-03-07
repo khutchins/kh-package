d51027a61e6a3f503e3f1cd5d0fb6bd94bb1a6ce
Kevin Hutchins <possessedcow@gmail.com>
2024-03-07 08:54:10 -0800

    Tweaks to minimize saving.

--- Runtime/Scripts/KH/Achievements/AchievementManager.cs
+++ Runtime/Scripts/KH/Achievements/AchievementManager.cs
@@ -13,7 +13,9 @@ namespace KH.Achievements {
     public class AchievementManager : SingletonInstance<AchievementManager> {
         [SerializeField] StringSignal UnlockSignal;
 
+        public bool ManualSavesOnly = false;
         private HashSet<string> _achievementStatus = new HashSet<string>();
+        private List<string> _deferredAchievements = new List<string>();
         private IAchievementPersister _persistance;
 
         protected override void Awake() {
@@ -25,20 +27,61 @@ namespace KH.Achievements {
             LoadFromDisk();
         }
 
-        public void Unlock(string id, bool silent) {
-            if (!silent && UnlockSignal != null) {
+        /// <summary>
+        /// Tells the manager to unlock the achievement. This will automatically save the new
+        /// achievement list to disk, unless <see cref="ManualSavesOnly"/> is enabled.
+        /// </summary>
+        /// <param name="id">Achievement id to unlock.</param>
+        /// <param name="deferNotifications">If true, will not notify about the unlocked 
+        /// achievement until <see cref="FlushDeferredNotifications"/> is called. Useful if an 
+        /// achievement would ruin a moment. Does not interfere with saving.</param>
+        public void Unlock(string id, bool deferNotifications) {
+            if (_achievementStatus.Contains(id)) return;
+
+            if (!deferNotifications) {
+                Notify(id);
+            } else if (deferNotifications) {
+                _deferredAchievements.Add(id);
+            }
+
+            MaybeSave();
+        }
+
+        /// <summary>
+        /// Notifies listeners about all deferred notifications.
+        /// </summary>
+        public void FlushDeferredNotifications() {
+            foreach (string id in _deferredAchievements) {
+                Notify(id);
+            }
+            _deferredAchievements.Clear();
+        }
+
+        private void Notify(string id) {
+            if (UnlockSignal != null) {
                 UnlockSignal.Raise(id);
             }
         }
 
+        private void MaybeSave() {
+            if (ManualSavesOnly) return;
+            SaveToDisk();
+        }
+
         public void Lock(string id) {
+            if (!_achievementStatus.Contains(id)) return;
             _achievementStatus.Remove(id);
+            MaybeSave();
         }
 
         public void LockAll() {
             _achievementStatus.Clear();
         }
 
+        /// <summary>
+        /// Triggers a manual save to disk. Calling this is only necessary if automatic
+        /// saving is disabled via <see cref="ManualSavesOnly"/>.
+        /// </summary>
         public void SaveToDisk() {
             _persistance.Save(_achievementStatus);
         }
