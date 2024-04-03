dfb13b9e48c67d73694af113cbfcce73bec8be2b
Kevin Hutchins <possessedcow@gmail.com>
2024-04-03 09:01:14 -0700

    Working on better console command (descriptions + in-command autocomplete).

--- Runtime/Scripts/KH/Console/ConsoleManager.cs
+++ Runtime/Scripts/KH/Console/ConsoleManager.cs
@@ -17,7 +17,7 @@ namespace KH.Console {
         [Tooltip("Whether ` + shift will toggle console and ESC will close it. Otherwise, do it yourself (it implements IMenu, so you can use that).")]
         [SerializeField] bool UseDefaultDisplayControls = true;
 
-        private Dictionary<string, System.Func<string[], string>> _registeredCmds = new Dictionary<string, System.Func<string[], string>>();
+        private Dictionary<string, Command> _registeredCmds = new Dictionary<string, Command>();
         private Trie _trie = new Trie();
         private bool _isUp = false;
         private Canvas _canvas;
@@ -27,26 +27,111 @@ namespace KH.Console {
             _canvas = GetComponent<Canvas>();
             _canvas.enabled = false;
             INSTANCE = this;
-            RegisterHandler("getver", (string[] cmd) => {
-                return Application.version;
+            RegisterHandler(new Command() {
+                Name = "getver",
+                Description = "Gets the current build version.",
+                RunCallback = (string[] cmd) => {
+                    return Application.version;
+                }
             });
 
-            RegisterHandler("help", (string[] cmd) => {
-                string commands = string.Join(' ', _registeredCmds.Keys.OrderBy(a => a));
-                return $"Available commands: {commands}";
+            RegisterHandler(new Command() {
+                Name = "help",
+                Description = "Prints out the list of commands if no argument is given, prints out description if one is given. You know this, as you can only get this description by using it.",
+                RunCallback = (string[] cmd) => {
+                    if (cmd.Length <= 1) {
+                        string commands = string.Join(' ', _registeredCmds.Keys.OrderBy(a => a));
+                        return $"Available commands: {commands}";
+                    } else {
+                        if (_registeredCmds.TryGetValue(cmd[1], out Command value)) {
+                            if (string.IsNullOrWhiteSpace(value.Description)) {
+                                return $"No description for command: {value.Name}";
+                            } else {
+                                return $"{value.Name}: {value.Description}";
+                            }
+                        } else {
+                            return $"No command with name: {cmd[0]}";
+                        }
+                    }
+                },
+                Autocomplete = (string[] cmd) => {
+                    if (cmd.Length > 2) {
+                        return new string[0];
+                    } else {
+                        return _registeredCmds.Keys.ToArray();
+                    }
+                }
             });
         }
 
-        public void RegisterHandler(string command, System.Func<string[], string> callback) {
-            if (_registeredCmds.ContainsKey(command)) {
+        public void RegisterHandler(Command command) {
+            if (command.RunCallback == null || string.IsNullOrWhiteSpace(command.Name)) {
+                Debug.LogWarning($"Commands must have a valid Name and RunCallback specified. This will not be added.");
+                return;
+            }
+            if (_registeredCmds.ContainsKey(command.Name)) {
                 Debug.LogWarning($"Console already recognizes command '{command}'. Existing handler will be overwritten.");
             }
-            _registeredCmds[command] = callback;
-            _trie.Insert(command);
+            _registeredCmds[command.Name] = command;
+            _trie.Insert(command.Name);
+
+        }
+
+        public void RegisterHandler(string command, System.Func<string[], string> callback) {
+            RegisterHandler(new Command() {
+                Name = command,
+                RunCallback = callback,
+            });
+        }
+
+        public void UnregisterHandler(Command command) {
+            UnregisterHandler(command.Name);
         }
 
         public void UnregisterHandler(string command) {
             _registeredCmds.Remove(command);
+        }
+
+        private void HandleAutcomplete() {
+            // TODO: This doesn't work properly if autocomplete is triggered after a space (e.g. "help ").
+            // It thinks it's autocompleting the first token, so it does nothing. In this case, it should
+            // realize that the string ends with whitespace and treat it as an empty string in the second token.
+            string[] tokens = ParseText(_currentText).ToArray();
+            if (tokens.Length < 1) {
+                return;
+            }
+
+            string last = tokens[tokens.Length - 1];
+            if (last == null) last = "";
+            Trie trie = null;
+            if (tokens.Length == 1) {
+                trie = _trie;
+            } else {
+                string cmd = tokens[0];
+                if (!string.IsNullOrWhiteSpace(cmd) && _registeredCmds.ContainsKey(cmd)) {
+                    Command command = _registeredCmds[cmd];
+                    if (command.Autocomplete != null) {
+                        List<string> autocomplete = command.Autocomplete(tokens).ToList();
+                        if (autocomplete.Count > 0) {
+                            trie = new Trie();
+                            foreach (string val in autocomplete) {
+                                trie.Insert(val);
+                            }
+                        }
+                    }
+                }
+            }
+            if (trie != null) {
+                string shortestShared = _trie.GetShortestSharedPrefix(last);
+                List<string> words = _trie.WordsWithPrefix(_currentText).OrderBy(x => x).ToList();
+                string complete = string.Join(' ', tokens, 0, tokens.Length - 1) + " " + shortestShared;
+                if (words.Count > 1) {
+                    SetCurrentText(complete);
+                    OutputText.text = string.Join(' ', words);
+                } else {
+                    SetCurrentText(complete + " ");
+                }
+            }
         }
 
         private void Update() {
@@ -64,22 +149,9 @@ namespace KH.Console {
                 }
             }
             if (UnityEngine.Input.GetKeyDown(KeyCode.Tab)) {
-                // This key doesn't show up in inputString, for whatever reason, so it has
+                // Tab doesn't show up in inputString, for whatever reason, so it has
                 // to be handled here.
-                string[] tokens = ParseText(_currentText).ToArray();
-                if (tokens.Length != 1) {
-                    return;
-                }
-                string last = tokens[0];
-                if (last == null) last = "";
-                string shortestShared = _trie.GetShortestSharedPrefix(last);
-                List<string> words = _trie.WordsWithPrefix(_currentText).OrderBy(x => x).ToList();
-                if (words.Count > 1) {
-                    SetCurrentText(shortestShared);
-                    OutputText.text = string.Join(' ', words);
-                } else {
-                    SetCurrentText(shortestShared + " ");
-                }
+                HandleAutcomplete();
             }
 
             foreach (char c in UnityEngine.Input.inputString) {
@@ -123,7 +195,7 @@ namespace KH.Console {
             } else {
                 var handler = _registeredCmds[cmd[0]];
                 try {
-                    output += handler(cmd);
+                    output += handler.RunCallback(cmd);
                 } catch (System.Exception e) {
                     output += e.Message;
                 }
@@ -261,5 +333,12 @@ namespace KH.Console {
                 yield return remainder;
             }
         }
+    }
+
+    public struct Command {
+        public string Name;
+        public string Description;
+        public System.Func<string[], string> RunCallback;
+        public System.Func<string[], IEnumerable<string>> Autocomplete;
     }
 }
