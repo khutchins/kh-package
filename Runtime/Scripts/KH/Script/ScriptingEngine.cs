e9384eb752a8fbd54cac68e955c06dfdc04bf8ef
Kevin Hutchins <possessedcow@gmail.com>
2025-08-11 21:55:02 -0700

    Set DefaultExecutionOrder on scripting engine to be high prio.

--- Runtime/Scripts/KH/Script/ScriptingEngine.cs
+++ Runtime/Scripts/KH/Script/ScriptingEngine.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[DefaultExecutionOrder(-99)]
 public class ScriptingEngine : MonoBehaviour {
     public static ScriptingEngine INSTANCE;
     [SerializeField] CommandChannel Channel;
