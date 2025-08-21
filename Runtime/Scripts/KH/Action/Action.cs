e1bfffc6aaf6f0bce470b9776adce37f8a1879c4
Kevin Hutchins <possessedcow@gmail.com>
2025-08-20 22:02:32 -0700

    Support awaiting on actions.

--- Runtime/Scripts/KH/Action/Action.cs
+++ Runtime/Scripts/KH/Action/Action.cs
@@ -14,5 +14,26 @@ namespace KH.Actions {
 		protected void Finished() {
 			FinishedAction?.Invoke(this);
 		}
-	}
+
+        public IEnumerator BeginAndAwait() {
+            bool finished = false;
+
+            void handler(Action action) {
+                if (action == this) finished = true;
+            }
+
+            // Have to subscribe before finishing in case the action completes immediately.
+            FinishedAction += handler;
+            Begin();
+
+            // Only yield if not already finished.
+            if (!finished) {
+                while (!finished) {
+                    yield return null;
+                }
+            }
+
+            FinishedAction -= handler;
+        }
+    }
 }
