1e7ac0260b61ef369eb2746213541dd7fa888f82
Kevin Hutchins <possessedcow@gmail.com>
2025-08-20 22:48:13 -0700

    Remove some unnecessary comments.

--- Editor/Scripts/KH/Script/KVBSImporter.cs
+++ Editor/Scripts/KH/Script/KVBSImporter.cs
@@ -8,16 +8,13 @@ namespace KH.Script {
     [ScriptedImporter(version: 1, ext: "kvbs", AllowCaching = true)]
     public class KHScriptImporter : ScriptedImporter {
         public override void OnImportAsset(AssetImportContext ctx) {
-            // Read file as UTF8 (with BOM tolerance)
             var bytes = File.ReadAllBytes(ctx.assetPath);
             var text = Encoding.UTF8.GetString(bytes);
 
-            // Create the ScriptableObject and populate it
             var asset = ScriptableObject.CreateInstance<KVBSAsset>();
             asset.name = Path.GetFileNameWithoutExtension(ctx.assetPath);
             asset.SetText(text);
 
-            // Add to asset database
             ctx.AddObjectToAsset("KHScriptAsset", asset);
             ctx.SetMainObject(asset);
         }
